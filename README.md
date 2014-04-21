1bb9cc664b9b9f2b1787285fbdad27b924cfa9ba
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2014-04-21 04:46:43 +0200

    Fixed option parsing for spawn.
    * it is now possible to set a custom entry point.
    * argc and argv are now passed correctly.

--- README.md
+++ README.md
@@ -27,13 +27,15 @@ Plugins
 
 Plugins are shared objects.
 Plugins should read on standard input and write on standard output.
-The entry point of a plugin must take an argument count and an argument vector as parameters. A sample hello world plugin is included.
+The entry point of a plugin must take an argument count and an argument vector as parameters. By default, "main" is used as entry point, although this may be overriden using the "-e" argument. A sample hello world plugin is included.
 A plugin can be forked off using the spawn command:
 
 Syntax:
-    ventctl spawn <plugin>
+
+    ventctl spawn <plugin> [-eENTRY | --entry=ENTRY] [[arguments]]
 
 Example:
+
     ventctl spawn ./plugins/hello.so 
 
 
--- src/spawn.c
+++ src/spawn.c
@@ -5,6 +5,36 @@
 #include <sys/types.h>
 #include <signal.h>
 #include <dlfcn.h>
+#include <argp.h>
+
+struct spawn_arguments {
+  char *entry;
+  int argc;
+  char **argv;
+};
+
+int spawn_option_parse(int key, char *arg, struct argp_state *state) {
+  struct spawn_arguments *arguments = state->input;
+  switch(key) {
+    case 'e':
+      arguments->entry = arg;
+      printf("entry: %s\n", arg);
+      break;
+
+    case ARGP_KEY_ARG:
+      if (!arguments->argv)
+        arguments->argv = malloc((state->arg_num+1)*sizeof( char* ));
+      else
+        arguments->argv = realloc(arguments->argv, (state->arg_num+1)*sizeof( char* ));
+      arguments->argv[state->arg_num] = arg;
+      arguments->argc = state->arg_num;
+      break;
+
+    default:
+      return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
 
 void sig_chld(int signo) {
   int status, child_val;
@@ -20,31 +50,48 @@ void sig_chld(int signo) {
   }
 }
 
-
 command(spawn) {
-  void *plugin = dlopen(argv[1], RTLD_LAZY);
+  /* Option Parsing */
+  struct spawn_arguments arguments;
+  arguments.argc=0;
+  arguments.entry="main";
+  arguments.argv = NULL;
+  static struct argp_option options[] = {
+    {"entry", 'e', "SYMBOL", 0, "Use SYMBOL as entry point"},
+    {0}
+  };
+
+  static struct argp argp = { options, spawn_option_parse, 0, 0 }; 
+  if (argp_parse (&argp, argc+1, argv, ARGP_SILENT | ARGP_NO_ERRS, 0, &arguments)) {
+    syslog(LOG_WARNING, "could not parse options, using defaults");
+    arguments.argc = argc-1;
+    arguments.argv = &(argv[1]);
+  }
+  char *object = arguments.argv[0];
+
+  void *plugin = dlopen(object, RTLD_LAZY);
   if (!plugin)
-    syslog(LOG_ERR, "Unable to load \'%s\'", argv[1]);
+    syslog(LOG_ERR, "Unable to load \'%s\'", object);
   else {
-    void* initializer = dlsym(plugin,(argc==2)?argv[2]:"main");
+    void* initializer = dlsym(plugin, arguments.entry);
     if (initializer == NULL) {
-      syslog(LOG_ERR, "Unable to extract initializer \'%s\' from \'%s\'", (argc==2)?argv[2]:"main", argv[1]);
+      syslog(LOG_ERR, "Unable to extract initializer \'%s\' from \'%s\'", arguments.entry, object);
       } else {
         int (*plugin_main)(int argc, char **argv);
         plugin_main = (int (*)(int argc, char **argv)) initializer;
         signal(SIGCHLD, sig_chld);
         pid_t pid = fork();
         if (pid==0){ 
-          plugin_main(argc-1, &(argv[1]));
+          plugin_main(arguments.argc, arguments.argv);
           // C99 exit here without calling the atexit handler
           syslog(LOG_NOTICE, "terminated.");
           _Exit(0); 
         }
         else if (pid<0)
-          syslog(LOG_ERR, "could not fork %s", argv[1]);
+          syslog(LOG_ERR, "could not fork %s", object);
         else
-          printf("forked %s as pid %d\n", argv[1], pid);
-          syslog(LOG_NOTICE, "forked %s as pid %d", argv[1], pid);
+          printf("forked %s as pid %d\n", object, pid);
+          syslog(LOG_NOTICE, "forked %s as pid %d", object, pid);
       }
   }
 }
